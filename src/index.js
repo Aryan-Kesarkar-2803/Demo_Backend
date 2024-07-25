449d80feec738f6e9b8aaf2e6f051ea9c31089bb
AryanKesarkar2803 <aryankesarkar0033@gmail.com>
2024-07-25 23:11:53 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import mongoose from "mongoose";
+import express from 'express'
 import dotenv from 'dotenv'
 import { DB_NAME } from "./constants";
 dotenv.config()
+
+const app = express()
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server started at ${process.env.PORT}`)
+})
+
 async function connectDB(){
     await mongoose.connect(`${MONGODB_URL}/${DB_NAME}`)
     .then(()=>{
@@ -11,3 +19,5 @@ async function connectDB(){
         console.log(`Error MongoDB - ${err}`)
     })
 }
+
+connectDB();
