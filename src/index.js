ee75aab5657240afaf4010c1110f5d89a17c05f4
AryanKesarkar2803 <aryankesarkar0033@gmail.com>
2024-07-26 12:21:22 +0530

    Added DB

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import mongoose from "mongoose";
+
 import express from 'express'
 import dotenv from 'dotenv'
-import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js";
 dotenv.config()
 
 const app = express()
@@ -10,14 +10,16 @@ app.listen(process.env.PORT,()=>{
     console.log(`Server started at ${process.env.PORT}`)
 })
 
-async function connectDB(){
-    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-    .then(()=>{
-        console.log('Database Connected');
-    })
-    .catch((err)=>{
-        console.log(`Error MongoDB - ${err}`)
-    })
-}
-
 connectDB();
+
+// async function connectDB(){
+//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//     .then(()=>{
+//         console.log('Database Connected');
+//     })
+//     .catch((err)=>{
+//         console.log(`Error MongoDB - ${err}`)
+//     })
+// }
+
+// connectDB();
